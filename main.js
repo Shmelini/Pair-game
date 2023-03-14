3762dc8c0345792c24c7cb7523974f515972cb7e
Artem Bondarchuk <arhipilagys@gmail.com>
2023-03-14 19:02:13 +1100

    Комментарии

--- main.js
+++ main.js
@@ -24,37 +24,43 @@ function initCardsArray(lenght) {
 function createGame() {
     let table = createTable();
     let cards = initCardsArray(8);
+    let curCardsVal = [];
     for (let i = 0; i < cards.length; i++) {
         let div = document.createElement('li');
-        
         div.classList.add('card');
         div.textContent = cards[i];
-        div.setAttribute('data-value', div.textContent)
-        // Тут я создал массив, куда хочу пихать значения карт
-        let curCardsVal = [];
-        // Тут то, что происходит по нажатию на карты
         div.addEventListener('click', ()=> {
             if (document.getElementsByClassName("turned").length == 0 || document.getElementsByClassName("turned").length == 1) {
                     div.classList.toggle('turned')
-                    // Я хуй пойму, что происходит с этим массивом по нажатию. Он либо множится, либо значение в нем перезаписывается к хуям
-                    curCardsVal.push(div.dataset.value)
+                    curCardsVal.push(div.textContent)
             }
             else {
                 div.classList.remove('turned')
             }
+// Вот здесь я пробую сделать механику, где 2 одинаковых карты остаются открытыми, но работает оно только по факту клика, что хуйня
+
+            if (curCardsVal.length > 2 && curCardsVal[0] !== curCardsVal[1]) {
+                curCardsVal = []
+                div.classList.remove('turned')
+            }
             console.log(curCardsVal)
+
+            if (curCardsVal[0] === curCardsVal[1]) {
+                document.getElementsByClassName('turned').classList.add('done')
+            }
         });
-        table.append(div);  
+        table.append(div);
+        
     }; 
 };
 
 createGame()
 
-function ruleNumeroUno() {
-    document.getElementsByClassName("turned").length
-}
+// function ruleNumeroUno() {
+//     document.getElementsByClassName("turned").length
+// }
 
-ruleNumeroUno();
+// ruleNumeroUno();
 
 
 
